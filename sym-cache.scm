97482354b436514c3b1b9dd1fba5a65fdf359e43
Vladimir Zhbanov <vzhbanov@gmail.com>
2016-03-04 17:10:28 +0300

    Fixed attribute attachment in sym-cache.scm.

--- sym-cache.scm
+++ sym-cache.scm
@@ -29,17 +29,38 @@
     (lambda () (display (page->string page))))
   page)
 
+;;; For some reason, the core function %attach-attrib doesn't have
+;;; an option which would let us preserve attrib color. Hence, we
+;;; do this this explicitly here.
+(define (attach-attrib-preserving-color! object attrib)
+  (let ((color (object-color attrib)))
+    (attach-attribs! object attrib)
+    (set-object-color! attrib color)))
+
+(define (copy-object-with-attachment! object #;to page)
+  (and (not (attrib-attachment object))
+       (let ((new-object (copy-object object))
+             (new-attribs (map copy-object (object-attribs object))))
+         (page-append! page new-object)
+         (when (not (null? new-attribs))
+           (apply page-append! page new-attribs)
+           ;; The following call would сorrupt attrib color info
+           ;;   (apply attach-attribs! new-object new-attribs)
+           ;; See comments for attach-attrib-preserving-color!
+           (map
+            (lambda (attrib)
+              (attach-attrib-preserving-color! new-object attrib))
+            new-attribs)))))
+                                        ;
+
 ;;; Saves symbol BASENAME to the cache directory
 (define (cache-symbol basename)
   (let ((page (make-page basename))
         (component (make-component/library basename '(0 . 0) 0 #f #f)))
-    (close-page!
-     (page->file
-      (apply
-       page-append!
-       page
-       (map copy-object (component-contents component)))
-      (get-cache-name basename)))))
+    (map (lambda (object)
+           (copy-object-with-attachment! object #;to page))
+         (component-contents component))
+    (close-page! (page->file page (get-cache-name basename)))))
 
 ;;; Get a list of unique component basenames of PAGE
 (define (get-unique-component-names page)
