816846f74da673b82a0ea135edf56f27e3ee58bc
Vladimir Zhbanov <vzhbanov@gmail.com>
2016-03-14 16:47:24 +0300

    More verbose output to log.

--- examples/cache.scm
+++ examples/cache.scm
@@ -8,15 +8,15 @@
 ;;; Enable symbol cache in the directory
 (enable-symbol-cache "sym")
 
-;;; Use hooks to cache symbols automatically after page saving
-(add-hook!
- post-save-page-hook
- (lambda ()
-   (gschem-log "Caching used symbols.\n")
-   (cache-page-symbols (active-page))))
+;;; Caching procedure
+(define (cache)
+  (if (is-symbol-cache-enabled?)
+      (begin (gschem-log "Caching used symbols...\n")
+             (if (cache-page-symbols (active-page))
+                 (gschem-log "... caching completed.\n")
+                 (gschem-msg "Something went wrong with symbol caching.\nCheck log for more information.\n")))
+      (gschem-log "Caching symbols disabled.\n")))
 
-(add-hook!
- post-save-page-as-hook
- (lambda ()
-   (gschem-log "Caching used symbols.\n")
-   (cache-page-symbols (active-page))))
+;;; Use hooks to cache symbols automatically after page saving
+(add-hook! post-save-page-hook cache)
+(add-hook! post-save-page-as-hook cache)
