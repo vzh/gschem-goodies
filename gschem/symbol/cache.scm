60f1e0f0031349b8cd69860a19803422e4962332
Vladimir Zhbanov <vzhbanov@gmail.com>
2016-03-15 16:32:32 +0300

    Removed unnecessary procedure redefinition.

--- gschem/symbol/cache.scm
+++ gschem/symbol/cache.scm
@@ -8,8 +8,6 @@
                disable-symbol-cache
                is-symbol-cache-enabled?))
 
-(define geda-cache-page-symbols cache-page-symbols)
-
 ;;; Since gschem doesn't export embedded procedures properly, we
 ;;; won't be polite with it, either.
 ;;; This temporary procedure will be replaced with a proper one
@@ -17,14 +15,14 @@
 (define log! (@@ (guile-user) gschem-log))
 
 ;;; Redefine cache-page-symbols
-(define (cache-page-symbols)
+(define (cache!)
   (if (is-symbol-cache-enabled?)
       (begin (log! "Caching used symbols...\n")
-             (if (geda-cache-page-symbols (active-page))
+             (if (cache-page-symbols (active-page))
                  (log! "... caching completed.\n")
                  (log! "Something went wrong with symbol caching.\nCheck log for more information.\n")))
       (log! "Caching symbols disabled.\n")))
 
 ;;; Use hooks to cache symbols automatically after page saving
-(add-hook! post-save-page-hook cache-page-symbols)
-(add-hook! post-save-page-as-hook cache-page-symbols)
+(add-hook! post-save-page-hook cache!)
+(add-hook! post-save-page-as-hook cache!)
