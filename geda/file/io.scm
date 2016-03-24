d5eaec226b9990ce4a2fba7d327445ce64d2fe20
Vladimir Zhbanov <vzhbanov@gmail.com>
2016-03-24 14:14:50 +0300

    Add first version of the file->page procedure.
    Just for the history.

--- geda/file/io.scm
+++ geda/file/io.scm
@@ -1,6 +1,8 @@
 (define-module (geda file io)
   #:export (page->file)
-  #:use-module (geda page))
+  #:export (file->page)
+  #:use-module (geda page)
+  #:use-module (ice-9 lineio))
 
 ;;; Outputs schematic PAGE to file NAME
 ;;; Returns PAGE
@@ -8,3 +10,17 @@
   (with-output-to-file name
     (lambda () (display (page->string page))))
   page)
+
+;;; Reads schematic FILE and outputs a page named FILE
+(define (file->page file)
+  (define (file->string file)
+    (let* ((port (make-line-buffering-input-port (open-file file "r"))))
+      (do ((line "" (read-string port))
+           (s "" (string-append s line)))
+          ((eof-object? line) ; test
+           (close-port port)  ; expression(s) to evaluate in the end
+           s)                 ; return value
+                              ; empty body
+        )))
+
+  (string->page file (file->string file)))
