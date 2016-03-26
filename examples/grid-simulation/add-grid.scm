60c717a20f30038aba189d283efba3bbe64c1ca6
Vladimir Zhbanov <vzhbanov@gmail.com>
2016-03-26 23:12:31 +0300

    Removed obsolete file i/o procedures.

--- examples/grid-simulation/add-grid.scm
+++ examples/grid-simulation/add-grid.scm
@@ -1,11 +1,12 @@
-; Licence: same as geda-gaf
+; License: same as geda-gaf
 ; Author:  Vladimir Zhbanov vzhbanov@gmail.com
 
-(use-modules (ice-9 lineio))
-(use-modules (ice-9 rw))
-(use-modules (geda page))
-(use-modules (geda object))
-(use-modules (geda attrib))
+(use-modules (ice-9 lineio)
+             (ice-9 rw)
+             (geda page)
+             (geda object)
+             (geda attrib)
+             (geda file io))
 
 ; Setup grid steps and color numbers
 ; Please see gafrc for example of how to adjust colors
@@ -16,27 +17,6 @@
 (define mesh-grid-minor-color 22)
 (define mesh-grid-major-color 23)
 
-
-; Input/output procedures
-; reads FILE and outputs string
-(define (schfile->string inputf)
-  (let* ((port (make-line-buffering-input-port (open-file inputf "r"))))
-    (do ((line "" (read-string port))
-         (s "" (string-append s line)))
-      ((eof-object? line) ; test
-       (close-port port)  ; expression(s) to evaluate in the end
-       s)                 ; return value
-      ; empty body
-      )))
-
-; reads schematic file INPUTF and outputs PAGE object
-(define (schfile->page inputf)
-    (string->page inputf (schfile->string inputf)))
-
-; saves PAGE to file OUTPUTF
-(define (page->schfile page outputf)
-  (with-output-to-file outputf
-    (lambda () (display (page->string page)))))
 
 ; removes OBJECTS from OLD-PAGE and adds them to NEW-PAGE,
 ; thus letting us prepend objects to a page list of objects
@@ -86,7 +66,7 @@
 
     (let* ((input (cadr (command-line)))
            (output (caddr (command-line)))
-           (input-page (schfile->page input))
+           (input-page (file->page input))
            (output-page (make-page output))
            (objects (page-contents input-page))
            (bounds (apply object-bounds objects))
@@ -110,7 +90,7 @@
 
       (page-move-objects! input-page output-page objects)
 
-      (page->schfile output-page output)
+      (page->file output-page output)
       )))
 
 ; Run
